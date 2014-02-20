1db155fe0d0bb89457d3df82613f237732c56bb5
Ben <ben@valice.com>
2014-02-20 16:41:30 -0700

    Add cryptoSystem test

---
+++ Tests/CryptoSystem/AES_SHA1Test.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Tests\CryptoSystem;
+
+use Rosio\EncryptedCookie\CryptoSystem\AES_SHA1;
+
+class AES_SHA1Test extends \PHPUnit_Framework_TestCase
+{
+	protected $cryptoSystem;
+
+	function setUp ()
+	{
+		$this->cryptoSystem = new AES_SHA1('symkey', 'hmackey');
+	}
+
+	public function testEncryptDecrypt ()
+	{
+		$data = 'Some data. Special characters: ' . chr(1) . chr(2) . chr(3) . '.';
+
+		$encData = $this->cryptoSystem->encrypt($data);
+		$decData = $this->cryptoSystem->decrypt($data);
+
+		$this->assertEquals($data, $decData);
+	}
+}
