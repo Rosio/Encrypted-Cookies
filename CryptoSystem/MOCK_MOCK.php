731022ac88ec770e6fcdc366996eb2bf20900e87
Ben <ben@valice.com>
2014-02-20 16:20:42 -0700

    Add mock crypto system

---
+++ CryptoSystem/MOCK_MOCK.php
@@ -0,0 +1,19 @@
+<?php
+namespace Rosio\EncryptedCookie\CryptoSystem;
+
+class MOCK_MOCK
+{
+	public function __construct()
+	{
+	}
+
+	public function encrypt ($data)
+	{
+		return $data;
+	}
+
+	public function decrypt ($data)
+	{
+		return $data;
+	}
+}
