2a364262d4fb985121ea4d84e1d8ebf1f0a874a9
Ben <ben@valice.com>
2014-02-20 17:59:23 -0700

    Add test to check expiration

--- Tests/CryptoSystem/AES_SHA1Test.php
+++ Tests/CryptoSystem/AES_SHA1Test.php
@@ -3,6 +3,7 @@
 namespace Tests\CryptoSystem;
 
 use Rosio\EncryptedCookie\CryptoSystem\AES_SHA1;
+use Rosio\EncryptedCookie\Exception\InputExpiredException;
 
 class AES_SHA1Test extends \PHPUnit_Framework_TestCase
 {
@@ -21,5 +22,17 @@ class AES_SHA1Test extends \PHPUnit_Framework_TestCase
 		$decData = $this->cryptoSystem->decrypt($encData);
 
 		$this->assertEquals($data, $decData);
+	}
+
+	/**
+	 * @expectedException \Rosio\EncryptedCookie\Exception\InputExpiredException
+	 */
+	public function testExpiration ()
+	{
+		$data = 'Some data.';
+
+		$encData = $this->cryptoSystem->encrypt($data, 1);
+		sleep(2);
+		$decData = $this->cryptoSystem->decrypt($encData);
 	}
 }
