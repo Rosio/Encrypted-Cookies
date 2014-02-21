d6c24eaab4f4b3d487393b6de54787a52bf58fc8
Ben <ben@valice.com>
2014-02-20 17:03:34 -0700

    Finished basic encryption routine

--- src/CryptoSystem/AES_SHA1.php
+++ src/CryptoSystem/AES_SHA1.php
@@ -1,8 +1,12 @@
 <?php
 namespace Rosio\EncryptedCookie\CryptoSystem;
 
+use Rosio\EncryptedCookie\Exception\RGPUnavailable;
+
 class AES_SHA1
 {
+	const IV_SIZE = 16;
+
 	private $symmetricKey;
 	private $HMACKey;
 
@@ -14,7 +18,15 @@ class AES_SHA1
 
 	public function encrypt ($data)
 	{
-		return $data;
+		$iv = $this->getRandom(self::IV_SIZE);
+		$atime = time();
+		$tid = $this->getTID();
+
+		$encData = mcrypt_encrypt(MCRYPT_RIJNDAEL_256, $this->symmetricKey, $data, MCRYPT_MODE_CBC, $iv);
+
+		$mac = hash_hmac('sha1', base64_encode($encData) . base64_encode($atime) . base64_encode($tid) . base64_encode($iv), $this->HMACKey, true);
+
+		return base64_encode($encData) . '|' . base64_encode($atime) . '|' . base64_encode($tid) . '|' . base64_encode($iv) . '|' . base64_encode($mac);
 	}
 
 	public function decrypt ($data)
@@ -24,6 +36,26 @@ class AES_SHA1
 
 	protected function getRandom ($length)
 	{
-		return openssl_random_pseudo_bytes($length, true);
+		$wasCryptoSecure = false;
+		$random = openssl_random_pseudo_bytes($length, $wasCryptoSecure);
+
+		if ($wasCryptoSecure !== true)
+			throw new RGPUnavailable();
+
+		return $random;
+	}
+
+	function setIVSize ($size)
+	{
+		$this->IVSize = $size;
+	}
+
+	/**
+	 * Get a string which uniquely represents the algorithms and keys used to encrypt the data.
+	 * @return string
+	 */
+	function getTID ()
+	{
+		return substr(md5(md5($this->symmetricKey) . 'AES_SHA1' . md5($this->HMACKey)), 0, 8);
 	}
 }
