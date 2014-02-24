070cc1d17dfc6557314c554cb0fd923441796487
Ben <ben@valice.com>
2014-02-24 14:24:33 -0700

    Allow the factory to specify a cookieStorage object

--- src/CookieFactory.php
+++ src/CookieFactory.php
@@ -3,20 +3,28 @@
 namespace Rosio\EncryptedCookie;
 
 use Rosio\EncryptedCookie\CryptoSystem\iCryptoSystem;
+use Rosio\EncryptedCookie\CookieStorage;
 
 class CookieFactory
 {
 	private $cryptoSystem;
+	private $cookieStorage;
 
-	function __construct (iCryptoSystem $cryptoSystem)
+	function __construct (iCryptoSystem $cryptoSystem, CookieStorage $cookieStorage = null)
 	{
 		$this->cryptoSystem = $cryptoSystem;
+
+		if ($cookieStorage === null)
+			$this->cookieStorage = new CookieStorage;
+		else
+			$this->cookieStorage = $cookieStorage;
 	}
 
 	function create ($name)
 	{
 		$cookie = new EncryptedCookie ($name);
 		$cookie->setCryptoSystem($this->cryptoSystem);
+		$cookie->setCookieStorage($this->cookieStorage);
 
 		return $cookie;
 	}
@@ -24,7 +32,10 @@ class CookieFactory
 	function get ($name)
 	{
 		$cookie = new EncryptedCookie ($name);
-		$cookie->setCryptoSystem($this->cryptoSystem)->load();
+		$cookie->setCryptoSystem($this->cryptoSystem);
+		$cookie->setCookieStorage($this->cookieStorage);
+
+		$cookie->load();
 
 		return $cookie;
 	}
